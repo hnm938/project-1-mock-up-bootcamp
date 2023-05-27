5e9b124eef74f3f31bc560a4dad9a955ab6db258
hnm938 <mrbooomboom@gmail.com>
2023-05-26 22:38:33 -0400

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
+function showRecipe(
+  instructions,
+  ingredients,
+  measurements,
+  drinkName,
+  isAlcoholic,
+  drinkCategory,
+  glassType
+) {
+  const modal = document.getElementById("recipe-modal");
+  const drinkInfo = document.getElementById("recipe--drink-info");
+  const drinkIngredients = document.getElementById("recipe--drink-ingredients");
+
+  ingredients = ingredients.split(",");
+  measurements = measurements.split(",");
+
+  for (let ingredient in ingredients) {
+    ingredients[ingredient] = `<span style="font-family: 'Lulo Bold'">${
+      measurements[ingredient] == undefined ? "" : measurements[ingredient]
+    }</span> ${ingredients[ingredient]}`;
+  }
+
+  modal.showModal();
+
+  // Set drink name header
+  modal.querySelector("#recipe--drink-name").innerHTML = drinkName;
+  modal.querySelector("#recipe--instructions").innerHTML = instructions;
+  modal.querySelector("#recipe--glass-type").innerHTML = `Glass: ${glassType}`;
+
+  // Set drink ingredients
+  drinkIngredients.innerHTML = `${ingredients
+    .map((ingredient) => `<li>${ingredient}</li>`)
+    .join("")}`;
+
+  // Set drink info
+  drinkInfo.innerHTML = `
+    <li>${isAlcoholic}</li>
+    <li>${drinkCategory}</li>
+    <li></li>
+    <li></li>
+  `;
+}
+
 const drinkListContainer = document.getElementById("drink-list-container");
 
 async function fetchDrink(query) {
-  // queries: s= search by drink name, i= serach by ingredient name, f= search by first letter,  
+  // queries: s= search by drink name, i= serach by ingredient name, f= search by first letter,
 
   // Fetch from API using drink name
   const res = await fetch(
@@ -72,6 +115,55 @@ async function fetchDrink(query) {
       }
       //#endregion
 
+      let drinkMeasurements = [];
+      //#region Gather drink measurements
+      if (drink.strMeasure1) {
+        drinkMeasurements.push(drink.strMeasure1);
+      }
+      if (drink.strMeasure2) {
+        drinkMeasurements.push(drink.strMeasure2);
+      }
+      if (drink.strMeasure3) {
+        drinkMeasurements.push(drink.strMeasure3);
+      }
+      if (drink.strMeasure4) {
+        drinkMeasurements.push(drink.strMeasure4);
+      }
+      if (drink.strMeasure5) {
+        drinkMeasurements.push(drink.strMeasure5);
+      }
+      if (drink.strMeasure6) {
+        drinkMeasurements.push(drink.strMeasure6);
+      }
+      if (drink.strMeasure7) {
+        drinkMeasurements.push(drink.strMeasure7);
+      }
+      if (drink.strMeasure8) {
+        drinkMeasurements.push(drink.strMeasure8);
+      }
+      if (drink.strMeasure9) {
+        drinkMeasurements.push(drink.strMeasure9);
+      }
+      if (drink.strMeasure10) {
+        drinkMeasurements.push(drink.strMeasure10);
+      }
+      if (drink.strMeasure11) {
+        drinkMeasurements.push(drink.strMeasure11);
+      }
+      if (drink.strMeasure12) {
+        drinkMeasurements.push(drink.strMeasure12);
+      }
+      if (drink.strMeasure13) {
+        drinkMeasurements.push(drink.strMeasure13);
+      }
+      if (drink.strMeasure14) {
+        drinkMeasurements.push(drink.strMeasure14);
+      }
+      if (drink.strMeasure15) {
+        drinkMeasurements.push(drink.strMeasure15);
+      }
+      //#endregion
+
       return `
         <li>
           <div class="drink-list--drink">
@@ -99,6 +191,16 @@ async function fetchDrink(query) {
                 `
                 : ""
             }
+            <button class="recipe-button" onclick="showRecipe(
+              '${drink.strInstructions}', 
+              '${drinkIngredients}',
+              '${drinkMeasurements}',
+              '${drink.strDrink}', 
+              '${drink.strAlcoholic}',
+              '${drink.strCategory}',
+              '${drink.strGlass}'
+            )"
+            >I want this!</button>
           </div>
         </li>
         <hr/>
@@ -111,9 +213,14 @@ async function init() {
   // Set drink start letter categories
   const drinkLetters = document.getElementById("drink-letters-category");
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  drinkLetters.innerHTML = alphabet.split("").map((letter) => `
+  drinkLetters.innerHTML = alphabet
+    .split("")
+    .map(
+      (letter) => `
     <button onclick="fetchDrink('f=${letter}')">${letter}</button>
-  `).join("");
+  `
+    )
+    .join("");
 }
 
 window.onload = () => {
